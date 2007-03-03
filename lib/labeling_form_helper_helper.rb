616ac4f0130b7fc6b77ffee4d12a8454aeaccb54
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-03-03 02:12:33 +0000

    hrmm
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@280 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- lib/labeling_form_helper_helper.rb
+++ lib/labeling_form_helper_helper.rb
@@ -1,68 +1,69 @@
 module LabelingFormHelperHelper
   
   def self.included(base)
-    LabelingFormHelperHelper.instance_methods.each { |msg| base.send :private, msg }
+    # LabelingFormHelperHelper.instance_methods.each { |msg| base.send :private, msg }
   end
   
-  def extract_label_options!(args)
-    return {} unless args.last.is_a? Hash
+  private  
+    def extract_label_options!(args)
+      return {} unless args.last.is_a? Hash
 
-    options = args.last
-    {
-      :disabled => (options[:label] == false ? true : false),     
-      :text     => options.delete(:label),
-      :class    => options.delete(:label_class),
-      :id       => options.delete(:label_id),
-      :for      => options[:id],
-      :wrap     => options.delete(:label_wrap),
-      :after    => options.delete(:label_after)
-    }
-  end
-  
-  def extract_label_html!(label)
-    [:id, :class, :for].inject({}) { |html,k| html.merge k => label.delete(k) }
-  end
-  
-  def validate_after_option!(label)
-    if label[:after]
-      raise ArgumentError, ':label_after works in conjunction with :label_wrap => true'
+      options = args.last
+      {
+        :disabled => (options[:label] == false ? true : false),     
+        :text     => options.delete(:label),
+        :class    => options.delete(:label_class),
+        :id       => options.delete(:label_id),
+        :for      => options[:id],
+        :wrap     => options.delete(:label_wrap),
+        :after    => options.delete(:label_after)
+      }
     end
-  end
   
-  # We need to account for certain optional arguments
-  # that can occur before the options hash in the unlabeled helpers.
-  #
-  # Specifically, we want to be able to ignore them and say things like:
-  #     check_box_tag :bulk_delete, :label => false
-  def handle_disparate_args!(helper, args)
-    # Ignore the options hash, if present, until we are done munging the args.
-    options = args.pop if args.last.is_a? Hash
-
-    if args.size == 1
-      if check_or_radio? helper
-        args.insert 1, 1
-      # Everything except :file_field_tag takes something as its second
-      # argument which can be safely defaulted to +nil+.
-      elsif helper != :file_field_tag
-        args.insert 1, nil
+    def extract_label_html!(label)
+      [:id, :class, :for].inject({}) { |html,k| html.merge k => label.delete(k) }
+    end
+  
+    def validate_after_option!(label)
+      if label[:after]
+        raise ArgumentError, ':label_after works in conjunction with :label_wrap => true'
       end
     end
+  
+    # We need to account for certain optional arguments
+    # that can occur before the options hash in the unlabeled helpers.
+    #
+    # Specifically, we want to be able to ignore them and say things like:
+    #     check_box_tag :bulk_delete, :label => false
+    def handle_disparate_args!(helper, args)
+      # Ignore the options hash, if present, until we are done munging the args.
+      options = args.pop if args.last.is_a? Hash
 
-    # :check_box_tag and :radio_button_tag can take another argument
-    # to determine if they are 'checked' or not.
-    if args.size == 2
-      if check_or_radio? helper
-        args.insert 2, false
+      if args.size == 1
+        if check_or_radio? helper
+          args.insert 1, 1
+        # Everything except :file_field_tag takes something as its second
+        # argument which can be safely defaulted to +nil+.
+        elsif helper != :file_field_tag
+          args.insert 1, nil
+        end
       end
-    end
+
+      # :check_box_tag and :radio_button_tag can take another argument
+      # to determine if they are 'checked' or not.
+      if args.size == 2
+        if check_or_radio? helper
+          args.insert 2, false
+        end
+      end
     
-    # Reunite the options with the rest of the args.
-    args << options if options
-  end
+      # Reunite the options with the rest of the args.
+      args << options if options
+    end
 
-  # Returns true for +helper+ == :check_box_tag and :radio_button_tag
-  def check_or_radio?(helper)
-    [:check_box_tag, :radio_button_tag].include? helper.to_sym
-  end
+    # Returns true for +helper+ == :check_box_tag and :radio_button_tag
+    def check_or_radio?(helper)
+      [:check_box_tag, :radio_button_tag].include? helper.to_sym
+    end
   
 end
