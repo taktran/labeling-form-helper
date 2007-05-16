7bdd5bff0760d7a703304a7be7fd592a8d407dfb
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-05-16 21:10:08 +0000

    use define_method; tag helper still fucked
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@463 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- lib/labeling_form_builder.rb
+++ lib/labeling_form_builder.rb
@@ -10,41 +10,34 @@ class LabelingFormBuilder < ActionView::Helpers::FormBuilder
     %w( form_for fields_for hidden_field password_field file_field )
   end
   
-  new_helpers = labelable.inject('') do |defs,helper|
-    defs << %{
-      def #{helper}(*args)
-        label = extract_label_options! args
-        
-        # handle_disparate_args! :#{helper}, args
-
-        # allow access to original behavior using :label => false
-        unlabeled_tag = super
-        return unlabeled_tag if label[:disabled]
-        
-        label[:for]  ||= extract_for unlabeled_tag
-        label[:text] ||= args.first.to_s.humanize
-        
-        label_html = extract_label_html! label
-        
-        # return label and tag according to custom options
-        if label[:wrap]
-          label_and_tag = if label[:after]
-            [unlabeled_tag, label[:text]]
-          else
-            [label[:text], unlabeled_tag]
-          end.join("\n")
-          
-          @template.content_tag(:label, label_and_tag, label_html)
+  labelable.each do |helper|
+    define_method helper do |*args|
+      label = extract_label_options! args
+      
+      unlabeled_tag = super
+      return unlabeled_tag if label[:disabled]
+      
+      label[:for]  ||= extract_for unlabeled_tag
+      label[:text] ||= args.first.to_s.humanize
+      
+      label_html = extract_label_html! label
+      
+      # return label and tag according to custom options
+      if label[:wrap]
+        label_and_tag = if label[:after]
+          [unlabeled_tag, label[:text]]
         else
-          validate_after_option! label
-          
-          [@template.content_tag(:label, label[:text], label_html), unlabeled_tag].join("\n")
-        end
+          [label[:text], unlabeled_tag]
+        end.join("\n")
+        
+        @template.content_tag(:label, label_and_tag, label_html)
+      else
+        validate_after_option! label
+        
+        [@template.content_tag(:label, label[:text], label_html), unlabeled_tag].join("\n")
       end
-    }
+    end
   end
-  
-  class_eval new_helpers, __FILE__, __LINE__
   
   private
     def extract_for(tag)
--- lib/labeling_form_tag_helper.rb
+++ lib/labeling_form_tag_helper.rb
@@ -6,48 +6,43 @@ module LabelingFormTagHelper
   # A list of labelable helpers. We exclude password and file fields because they use text field,
   # so we would get double labels including them.
   def self.labelable
-    public_instance_methods.reject { |h| h =~ /form|submit|hidden|password|file/ }
+    public_instance_methods.reject { |h| h =~ /form|submit|hidden|password|file/ }.map(&:to_sym)
   end
   
-  new_helpers = labelable.inject('') do |defs, helper|    
-    defs << %{
-      def #{helper}_with_label(*args)
-      # def #{helper}(*args)
-        label = extract_label_options! args
-                
-        handle_disparate_args! :#{helper}, args
-        
-        # allow access to old behavior with :label => false
-        # unlabeled_tag = #{helper}_without_label *args
-        unlabeled_tag = #{helper}_without_label *args
-        return unlabeled_tag if label[:disabled]
-        
-        name = args.first.to_s
-        label[:text] ||= name.humanize
-        label[:for]  ||= extract_id name
-        
-        label_html = extract_label_html! label
-        
-        if label[:wrap]
-          label_and_tag = if label[:after]
-            [unlabeled_tag, label[:text]]
-          else
-            [label[:text], unlabeled_tag]
-          end.join("\n")
-          
-          content_tag(:label, label_and_tag, label_html)
-        else
-          validate_after_option! label
-          
-          [content_tag(:label, label[:text], label_html), unlabeled_tag].join("\n")
-        end
-      end
+  labelable.each do |helper|
+    define_method "#{helper}_with_label" do |*args|
+      label = extract_label_options! args
+              
+      handle_disparate_args! helper, args
       
-      alias_method_chain :#{helper}, :label
-    }
+      unlabeled_tag = send "#{helper}_without_label", *args
+      return unlabeled_tag if label[:disabled]
+      
+      name = args.first.to_s
+      label[:text] ||= name.humanize
+      label[:for]  ||= extract_id name
+      
+      label_html = extract_label_html! label
+      
+      if label[:wrap]
+        label_and_tag = if label[:after]
+          [unlabeled_tag, label[:text]]
+        else
+          [label[:text], unlabeled_tag]
+        end.join("\n")
+        
+        content_tag(:label, label_and_tag, label_html)
+      else
+        validate_after_option! label
+        
+        [content_tag(:label, label[:text], label_html), unlabeled_tag].join("\n")
+      end
+    end
+      
+    alias_method_chain helper, :label
   end
   
-  module_eval new_helpers, __FILE__, __LINE__
+  # module_eval new_helpers, __FILE__, __LINE__
   
   private    
     def extract_id(name)
