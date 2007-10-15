3e649da6c868b71e81c369824a09df0ddb44d34e
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-10-15 19:12:49 +0000

    maybe even this
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@491 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- init.rb
+++ init.rb
@@ -1,9 +1,13 @@
 require 'labeling_form_builder'
 require 'labeling_form_tag_helper'
 
-module ActionView::Helpers::FormTagHelper
+module ActionView
+module Helpers
+module FormTagHelper
   # Use to overwrite the default, non-instance based form helpers.
   def self.enable_labeling_form_helper!
     module_eval { include LabelingFormTagHelper }
   end
+end
+end
 end
