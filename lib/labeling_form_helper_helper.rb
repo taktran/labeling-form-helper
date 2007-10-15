2edee135c1c35023c3b13e7de533963c0d95517a
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-10-15 20:17:19 +0000

    nodoc
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@494 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- lib/labeling_form_helper_helper.rb
+++ lib/labeling_form_helper_helper.rb
@@ -1,7 +1,7 @@
-module LabelingFormHelperHelper
+module LabelingFormHelperHelper #:nodoc:
     
 private
-  def extract_label_options!(args)
+  def extract_label_options!(args) #:nodoc:
     options = args.last.is_a?(Hash) ? args.last : {}
     
     label = options.delete :label
@@ -19,15 +19,15 @@ private
     label
   end
   
-  def extract_label_html!(label)
+  def extract_label_html!(label) #:nodoc:
     [:id, :class, :for].inject({}) { |html,k| html.merge k => label.delete(k) }
   end
   
-  def validate_after_option!(label)
+  def validate_after_option!(label) #:nodoc:
     raise ArgumentError, ':label_after works in conjunction with :label_wrap => true' if label[:after]
   end
   
-  def check_or_radio?(helper)
+  def check_or_radio?(helper) #:nodoc:
     [:check_box_tag, :radio_button_tag].include? helper.to_sym
   end
 end
