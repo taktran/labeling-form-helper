f1dc74921e52ea250243bb194db5141e70d20360
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-10-15 18:38:24 +0000

    trying to reenable the tag helper again
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@488 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- init.rb
+++ init.rb
@@ -1,2 +1,9 @@
 require 'labeling_form_builder'
 require 'labeling_form_tag_helper'
+
+module ActionView::Base::Helpers
+  # Use to overwrite the default, non-instance based form helpers.
+  def self.use_labeling_form_helper!
+    FormTagHelper.send :include, LabelingFormTagHelper
+  end
+end
