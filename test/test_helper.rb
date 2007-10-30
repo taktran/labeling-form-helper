3b83dda3c907c020f3fa96e18c4d03126a77fc3a
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2007-10-30 04:02:33 +0000

    remove mocha/stubba since they are not in use yet
    git-svn-id: http://sethrasmussen.com/svn/rails/plugins/labeling_form_helper@532 fb9bd3d9-41f6-4e67-aeff-99515e7e2596

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,2 @@
-%w( rubygems active_support action_controller action_view mocha stubba test/unit ).each { |x| require x }
+%w( rubygems active_support action_controller action_view test/unit ).each { |x| require x }
 $:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
